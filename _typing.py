38daeac3ba92b230b3cf913d48dbb649cc16bcd6
Chitaoji <2360742040@qq.com>
2025-06-03 12:03:19 +0800

    Update _typing.py

--- _typing.py
+++ _typing.py
@@ -1,20 +1,17 @@
 """
 Contains typing classes.
 
-NOTE: this module is private. All functions and objects are available in the main
-`re_extensions` namespace - use that instead.
+NOTE: this module is not intended to be imported at runtime.
 
 """
 
-import logging
 from typing import TYPE_CHECKING
+
+import loggings
 
 if TYPE_CHECKING:
     from re import Match, Pattern, RegexFlag
 
     from .src._typing import FlagType, MatchType, PatternType, ReplType
 
-logging.warning(
-    "importing from '._typing' - this module is not intended for direct import, "
-    "therefore unexpected errors may occur"
-)
+loggings.warning("this module is not intended to be imported at runtime")
--- src/_typing.py
+++ src/_typing.py
@@ -1,23 +1,20 @@
 """
 Contains typing classes.
 
-NOTE: this module is private. All functions and objects are available in the main
-`re_extensions` namespace - use that instead.
+NOTE: this module is not intended to be imported at runtime.
 
 """
 
-import logging
 from typing import TYPE_CHECKING, Callable, Union
+
+import loggings
 
 if TYPE_CHECKING:
     from re import Match, Pattern, RegexFlag
 
     from .core import SmartMatch, SmartPattern
 
-logging.warning(
-    "importing from '._typing' - this module is not intended for direct import, "
-    "therefore unexpected errors may occur"
-)
+loggings.warning("this module is not intended to be imported at runtime")
 
 PatternType = Union[str, "Pattern[str]", "SmartPattern[str]"]
 MatchType = Union["Match[str]", "SmartMatch[str]", None]
